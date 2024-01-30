437fac0e57330864833506736bea9029ff644fb1
Declan Price <declanprice@gmail.com>
2024-01-30 00:18:38 +0000

    test: put item builder unit test added

---
+++ test/put-item-builder.spec.ts
@@ -0,0 +1,51 @@
+import {PutItemBuilder} from "../src/builders/put-item-builder";
+import {DynamoDBClient, PutItemCommand} from "@aws-sdk/client-dynamodb";
+import { mockClient } from "aws-sdk-client-mock";
+import 'aws-sdk-client-mock-jest';
+import {eq} from "../src/expressions/condition/eq";
+import {or} from "../src/expressions/condition/or";
+
+const testTableName = 'test_table';
+const testClient: any = mockClient(new DynamoDBClient());
+const validTestId = '123';
+const invalidTestId = '4444';
+
+describe('PutItemBuilder', () => {
+
+    beforeEach(() => {
+        testClient.reset();
+
+        testClient
+            .on(PutItemCommand)
+            .resolves()
+    })
+
+    it('should send PutItemCommand with configured options', async () => {
+        const builder = new PutItemBuilder(testTableName, testClient)
+
+        await builder.item({id: validTestId}).condition(
+            eq('name', 'dec'),
+            or(),
+            eq('age', 25)
+        ).exec();
+
+
+        expect(testClient).toHaveReceivedCommandWith(PutItemCommand, {
+            TableName: testTableName,
+            Item: {
+                id: {
+                    S: validTestId
+                }
+            },
+            ConditionExpression: '#0 = :1 or #2 = :3',
+            ExpressionAttributeNames: {
+                "#0": 'name',
+                "#2": 'age'
+            },
+            ExpressionAttributeValues: {
+                ":1": { S: 'dec' },
+                ":3": { N: '25' }
+            }
+        })
+    })
+})
