9fca8f485175d8beeacb35cdf34a792024788e41
Declan <declanprice1@gmail.com>
2024-01-30 14:15:21 +0000

    test: added some integration tests

--- integration/put-item.spec.ts
+++ integration/put-item.spec.ts
@@ -1,7 +1,8 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb'
+import { DeleteItemCommand, DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb'
 import { Store } from '../src/store'
 import { marshall } from '@aws-sdk/util-dynamodb'
 import { wait } from './wait'
+import { DeleteItemBuilder } from '../src/builders/delete-item-builder'
 
 const client = new DynamoDBClient()
 
@@ -22,17 +23,30 @@ describe('PutItem', () => {
     name: 'test'
   }
 
+  beforeEach(async () => {
+    await store.delete().key({ pk: customer.pk, sk: customer.sk }).exec()
+
+    await wait(100)
+  })
+
   it('should put item successfully', async () => {
     const item = await store.put<CustomerItem>().item(customer).exec()
     expect(item).toBeNull()
-    await wait(100)
-    const getItem = await store.get<CustomerItem>().key({ pk: customer.pk, sk: customer.sk }).exec()
+    const getItem = await store.get<CustomerItem>().key({ pk: customer.pk, sk: customer.sk }).consistent().exec()
     expect(getItem).toBeTruthy()
   })
 
-  it('should put item successfully and return values', async () => {
-    const item = await store.put<CustomerItem>().item(customer).returnOld().exec()
+  it('should put item successfully and return no values', async () => {
+    const item = await store.put<CustomerItem>().item(customer).exec()
     expect(item).toBeNull()
+  })
+
+  it('should put item successfully and return old values', async () => {
+    await store.put<CustomerItem>().item(customer).exec()
     await wait(100)
+    const item = await store.put<CustomerItem>().item(customer).returnOld().exec()
+    if (!item) throw new Error('item is null')
+    expect(item).toBeTruthy()
+    expect(item.pk).toEqual(customer.pk)
   })
 })
