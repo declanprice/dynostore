b111f77d27e727fbfa67fd14edc8157f57983742
Declan Price <declanprice@gmail.com>
2024-11-28 07:57:37 +1000

    remove docker folder from git

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/declanprice/Desktop/Github/dynostore/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/declanprice/Desktop/Github/dynostore/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/declanprice/Desktop/Github/dynostore/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/declanprice/Desktop/Github/dynostore/src/css/custom.css"),
-];
